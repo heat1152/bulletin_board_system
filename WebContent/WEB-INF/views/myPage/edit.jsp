b8de22b96a9dae9afabd254a0a6fe91acbbb0b9a
hitoshi.kawauchi <azxssg@gmail.com>
2020-10-23 00:43:16 +0900

    Add Recruitment class

--- WebContent/WEB-INF/views/myPage/edit.jsp
+++ WebContent/WEB-INF/views/myPage/edit.jsp
@@ -7,10 +7,10 @@
                 <h2>マイページ編集</h2>
                 <form method="POST" action="<c:url value='/user/update'/>">
                    <label for="name">名前</label><br />
-                   <input type="text" name="name" value="${user.name}" />
+                   <input type="text" name="name" value="${user.name}" maxlength='20'/>
                     <br /><br />
                     <label for="profile">自己紹介文</label><br />
-                    <textarea name="profile" rows="10" cols="50">${user.profile}</textarea>
+                    <textarea name="profile" rows="10" cols="50" maxlength='255'>${user.profile}</textarea>
                     <br /><br />
                     <input type="hidden" name="_token" value="${_token}" />
                     <div id="form_button">
---
+++ src/models/Recruitment.java
@@ -0,0 +1,81 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Table(name = "recruitments")
+
+@Entity
+public class Recruitment {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    @Lob
+    @Column(name = "contents",length = 255,nullable = false)
+    private String contents;
+
+    @Column(name = "created_at",nullable = false)
+    private Timestamp created_at;
+
+    @Column(name = "updated_at",nullable = false)
+    private Timestamp updated_at;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+
+
+
+}
